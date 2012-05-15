bfe25f2f1b7e05407bfa420f72ac62589aef066e
Sascha Schneider <foomy.code@arcor.de>
2012-05-15 22:22:53 +0200

    library added

---
+++ library/LinkFu/Controller/Abstract.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once APPLICATION_PATH . '/../library/Foo/Controller/Abstract.php';
+
+/**
+ * Base controller for Link-Fu.
+ *
+ * @author		Sascha Schneider <foomy.code@arcor.de>
+ *
+ * @category	library
+ * @package		LinkFu_Controller_Abstract
+ */
+class LinkFu_Controller_Abstract extends Foo_Controller_Abstract
+{
+	/**
+	 * @var	Mcp_Auth_Adapter
+	 */
+	protected $_auth;
+
+	/**
+	 * @var	string
+	 */
+	protected $_role;
+
+	/**
+	 * @var	Mcp_Controller_Plugin_Acl
+	 */
+	protected $_acl;
+
+	public function init()
+	{
+		parent::init();
+	}
+}
